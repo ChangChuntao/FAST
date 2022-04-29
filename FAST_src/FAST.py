dc7086169a14240f986a4d34c1a9c27db64b692a
FAST <1252443496@qq.com>
2022-04-29 13:32:47 +0800

    init

--- FAST_src/FAST.py
+++ FAST_src/FAST.py
@@ -10,7 +10,7 @@
 #                  *通过下载列表解压文件
 #                  新增P1C1、P1P2、P2C2、GRACE_SLR、BEIDOU_SLR、MGEX_WHU_OSB、GLO_IGL_sp3、GPS_IGS_clk_30s资源
 #                  by Chang Chuntao
-#                  Github push test 2022 04 29 13 31
+#                  Github push test 2022 04 29 13 32
 
 import sys
 from ARG_Mode import ARG_Mode
