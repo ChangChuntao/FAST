694d2884c86f0ee7f99e28072cd097478162b48d
FAST <1252443496@qq.com>
2022-04-30 15:51:26 +0800

    init

--- FAST_src/FAST_Time.py
+++ FAST_src/FAST_Time.py
@@ -5,10 +5,19 @@
 # Latest Version : 1.12
 # Creation Date  : 2022.03.27 - Version 1.00
 # Date           : 2022.04.30 - Version 1.12
+
+from FAST_Print import PrintGDD
 from help import fastSoftwareInformation
 from GNSS_Timestran import gnssTimesTran
 
 # 2022-04-30 : 引导 by Chang Chuntao -> Version : 1.00
 fastSoftwareInformation()
-print()
-gnssTimesTran()
+cont = "y"
+while True:  # 循环运行
+    if cont == "y" or cont == "Y":
+        print()
+        gnssTimesTran()
+        PrintGDD("运行结束，是否重新引导？(y)", "input")
+        cont = input("     ")
+    else:
+        break
